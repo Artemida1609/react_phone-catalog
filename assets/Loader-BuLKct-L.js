5666e7959a180a97855da0216d53ba4ce38413ae
Artem Skichko <artem.skichko@student.tuke.sk>
2025-04-12 09:37:35 +0200

    Updates

---
+++ assets/Loader-BuLKct-L.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-6VOx6d9t.js";const r="_loader_1hq93_13",e={loader:r},a=()=>s.jsx(s.Fragment,{children:s.jsx("span",{className:`${e.loader}`})});export{a as L};
--- assets/Loader-DlcXfFM1.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Bsbv2Cxv.js";const r="_loader_1hq93_13",e={loader:r},a=()=>s.jsx(s.Fragment,{children:s.jsx("span",{className:`${e.loader}`})});export{a as L};
